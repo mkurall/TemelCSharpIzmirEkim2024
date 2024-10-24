3c42befb9bda9dd0ac6ff61039c97acf7fa03774
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 02:51:23 +0300

    u10 diziler

---
+++ u10_diziler/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] sayilar = { 5, 8, 3, 9, 12 };
+
+Console.WriteLine("dizinin elemanları:");
+
+foreach(int s in sayilar)
+{
+    Console.Write($"{s},");
+}
+
+int[] sayilar2 = new int[5];//new bellekten yer ayırmak için kullanılır
+
+sayilar2[0] = 3;
+Console.WriteLine("\n\n2.Dizinin 1.indeksteki elemanı kaç olsun?");
+sayilar2[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n2.dizinin \\elemanları:");// (\) escape karakteri
+
+foreach(int s in sayilar2)
+{
+    Console.Write($"{s},");
+}
