2987d8fcbb2747f632a5e1749cb34b397aff06c3
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 01:11:07 +0300

    u04 örneği

--- u03_nothesaplama/Program.cs
+++ u03_nothesaplama/Program.cs
@@ -10,4 +10,16 @@ double final = Convert.ToDouble(Console.ReadLine());//klavyeden girilen veriler 
 double sonuc = v1 * 0.30 + v2 * 0.30 + final * 0.40; 
 
 Console.WriteLine("Ortalama = " + sonuc);
-Console.Write("Geçti Mi:" + (sonuc >= 70));
+
+Console.WriteLine("Geçti Mi:" + (sonuc >= 70));
+
+if(sonuc>=70)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("BAŞARILI");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("BAŞARISIZ");
+}
---
+++ u04_vki_hesaplama/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Boyunuz(cm)?");
+double boy = double.Parse(Console.ReadLine());
+
+Console.Write("Kilonuz(kg)?");
+double kilo = Convert.ToDouble(Console.ReadLine());
+
+//vki = kilo kg / (boy m) karesi
+
+double boy2 = boy/100;//metre cinsine çevirdik
+double vki = kilo / (boy2 * boy2);
+
+Console.WriteLine($"Vki Değeri = {vki:f2}"); //string interpolation
+
+if(vki<18.5)
+    Console.WriteLine("ZAYIF");
+else if(vki>=18.5 && vki<25)
+    Console.WriteLine("NORMAL");
+else if(vki>=25 && vki<30)
+    Console.WriteLine("KİLOLU");
+else if(vki>=30)
+    Console.WriteLine("AŞIRI KİLOLU");
