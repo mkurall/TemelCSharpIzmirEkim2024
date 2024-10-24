84441994824246ad6793cd3795958dd531dd558e
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 02:32:30 +0300

    u09 örnek

---
+++ u09_pin_kodu/Program.cs
@@ -0,0 +1,9 @@
+﻿string girilenPinKodu="";
+int deneme=1;
+do
+{
+    Console.WriteLine($"PİN Kodunu Girin({deneme}.deneme):");
+    girilenPinKodu = Console.ReadLine();
+    deneme++;
+} while (girilenPinKodu != "1234");
+
