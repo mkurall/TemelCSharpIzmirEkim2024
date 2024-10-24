0981cb67711e4adb43613cc101de5872eac36141
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 01:39:44 +0300

    first commit

---
+++ u02_veri_tipleri/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(5);//tamsayı (int)
+Console.WriteLine(4.5);//ondalıklkı sayı (double)
+Console.WriteLine("btk akademi");//metin
+Console.WriteLine('c');//karakter
+Console.WriteLine(true);//yada false
+
+var a = 25f;
+
+long s1 = 1;
+int s2 = (int)s1;
+
+int s3 = Convert.ToInt32("50");
+
