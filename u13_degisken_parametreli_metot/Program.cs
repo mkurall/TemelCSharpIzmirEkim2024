cf4201687e1ad1d88a3abf0c97ab3a132f157948
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 04:03:30 +0300

    değişken parametreli metotlar

---
+++ u13_degisken_parametreli_metot/Program.cs
@@ -0,0 +1,15 @@
+﻿double OrtalamaHesapla(params int[] sayilar)
+{
+    int toplam = 0;
+    foreach(int s in sayilar)
+    {
+        toplam+=s;
+    }
+
+    return toplam / (double)sayilar.Length; //int/int sonuç yine int  (6+5)/2 = 5
+}
+
+
+double sonuc = OrtalamaHesapla(3,4,8,9,10,11,12,13);
+
+Console.WriteLine($"Ortalama Sonucu = {sonuc}");
