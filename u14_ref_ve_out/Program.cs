b2fb7278f51250da569bd2371e0434ac255637aa
Mustafa KURAL <mustafakural@outlook.com>
2024-10-25 04:09:53 +0300

    ref ve out

---
+++ u14_ref_ve_out/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Degistir(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int x = 3, y = 5;
+
+Console.WriteLine($"x değeri = {x}, y değeri = {y}");
+
+Degistir(ref x, ref y);
+
+Console.WriteLine($"yeni --> x değeri = {x}, y değeri = {y}");
